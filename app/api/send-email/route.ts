58875c0800077784e92b2b910fe12e482362ca4f
Andrew Duncan <dredayduncan@gmail.com>
2024-09-23 21:04:00 +0000

    excluded code for sending emails

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,26 +1,26 @@
-import { Resend } from "resend";
-import WelcomeTemplate from '@/emails/WelcomeTemplate';
-import { NextResponse } from "next/server";
+// import { Resend } from "resend";
+// import WelcomeTemplate from '@/emails/WelcomeTemplate';
+// import { NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-	try{
+// export async function POST() {
+// 	try{
 
-		const name = 'Dre';
+// 		const name = 'Dre';
 
-	await resend.emails.send({
-		from: "dredayduncan@gmail.com",
-		to: "dredayduncan@gmail.com",
-		subject: "...",
-		react: WelcomeTemplate({ name })
-	});
+// 	await resend.emails.send({
+// 		from: "example@gmail.com",
+// 		to: "example@gmail.com",
+// 		subject: "...",
+// 		react: WelcomeTemplate({ name })
+// 	});
 
-	return NextResponse.json({message: 'Email sent'});
+// 	return NextResponse.json({message: 'Email sent'});
 
-	}
-	catch (error) {
-		return NextResponse.json({detail: error}, {status: 500});
-	}
+// 	}
+// 	catch (error) {
+// 		return NextResponse.json({detail: error}, {status: 500});
+// 	}
 	
-}
+// }
